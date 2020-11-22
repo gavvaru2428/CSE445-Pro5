f2d263ee08ce550ddb2d09b243668e60a11b8a97
Anoop Reddy Vutukuri <avutuku2@sundevils.asu.edu>
2020-11-22 10:14:48 -0700

    Anoop: Global.asax added

--- Global.asax.cs
+++ Global.asax.cs
@@ -1,11 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Security.Principal;
 using System.Threading;
 using System.Web;
 using System.Web.Security;
 using System.Web.SessionState;
+using System.Xml;
 
 namespace project5
 {
@@ -47,7 +51,27 @@ namespace project5
 
         protected void Application_Error(object sender, EventArgs e)
         {
-
+            Exception ex = Server.GetLastError();
+            string str = GetStringFromFile("Errors.xml");
+            string time = "_" + DateTime.Now.ToString() + "_";
+            if (str == "" || str == "File not exist" || str == null || str == "\r\n")
+            {
+                //Handle new or empty file
+                JObject obj = new JObject(new JProperty(time, JsonConvert.SerializeObject(ex.Message)));
+                XmlDocument doc = JsonConvert.DeserializeXmlNode(obj.ToString(), "root");
+                PutStringToFile("Errors.xml", doc.OuterXml);
+            }
+            else
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(str);
+                string jsonText = JsonConvert.SerializeXmlNode(doc, Newtonsoft.Json.Formatting.None, true);
+                //Create a new cart for user or append the order to previously existing cart 
+                JObject errors = JObject.Parse(jsonText);
+                errors.AddFirst(new JProperty(time, JsonConvert.SerializeObject(ex.Message)));
+                doc = JsonConvert.DeserializeXmlNode(errors.ToString(), "root");
+                PutStringToFile("Errors.xml", doc.OuterXml);
+            }
         }
 
         protected void Session_End(object sender, EventArgs e)
@@ -59,5 +83,40 @@ namespace project5
         {
 
         }
+
+        //Write to a file
+        public static void PutStringToFile(string fileName, string value)
+        {
+            string fLocation = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data"); // Get path from server root to current
+            fLocation = Path.Combine(fLocation, fileName); // From current to App_Data
+            if (!System.IO.File.Exists(fLocation))
+            {
+                File.Create(fLocation);
+            }
+            using (StreamWriter sw = File.CreateText(fLocation))
+            {
+                sw.WriteLine(value);
+                sw.Close();
+            }
+
+        }
+
+        //Read from a file
+        public static string GetStringFromFile(string fileName)
+        {
+            string fLocation = Path.Combine(HttpRuntime.AppDomainAppPath, @"App_Data"); // From server root to current
+            fLocation = Path.Combine(fLocation, fileName); // From current to App_Data
+            if (!File.Exists(fLocation))
+            {
+                return "File not exist";
+            }
+            using (StreamReader sr = File.OpenText(fLocation))
+            {
+                string s = sr.ReadToEnd();
+                sr.Close();
+                return s;
+            }
+        }
     }
+}
 }
